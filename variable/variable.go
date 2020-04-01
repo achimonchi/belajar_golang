e6643c1c493d7bd69b5fb4969cf28ddbb974b339
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2020-04-01 16:40:09 +0700

    fix variable.go

--- variable/variable.go
+++ variable/variable.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// global variabel
+var global = "Ini Variable Golbal"
+
 func main() {
 
 	var hello = "Hello World"
@@ -14,6 +17,34 @@ func main() {
 	nama = "Jovie"
 	fmt.Println(nama)
 
+	// pemanggilan ini hanya bisa di lakukan di dalam function / method
+
 	namaLengkap := "Reyhan Jovie"
 	fmt.Println(namaLengkap)
+
+	// int
+	var nilai int8
+	nilai = 100
+	fmt.Println(nilai)
+
+	// jika tidak di definisikan, maka
+	// akan bernilai int (cek di file tipe data)
+	nilai2 := 120
+	fmt.Println(nilai2)
+
+	fmt.Println(global)
+	x()
+
+	// variable constan
+	const company string = "NooBee"
+	fmt.Println(company)
+
+	// setelah di definisikan, constant variable tidak bisa
+	// di ubah lagi isinya
+	// company = "New NooBee"
+	// fmt.Println(company)
+}
+
+func x() {
+	fmt.Println(global)
 }
