13a91a9e10962a71da00b860269fad50e923fe9d
Reyhan Jovie <achimonchi.reyhan@gmail.com>
2020-04-01 17:25:05 +0700

    add increament

---
+++ operator/increament.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// untuk increament, sama seperti bahasa pemrograman
+	// yang lain
+	a := 1
+	a++
+
+	fmt.Println(a)
+
+	// untuk decreament juga sama
+	b := 10
+	b--
+
+	fmt.Println(b)
+
+}
